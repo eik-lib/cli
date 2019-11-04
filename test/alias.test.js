a3870d463bee8c137674389e61dd07f7b6dc9ae0
Richard Walker <digitalsadhu@gmail.com>
2019-11-04 16:13:51 +1300

    expand alias to include a test for map aliasing

--- test/alias.test.js
+++ test/alias.test.js
@@ -7,14 +7,14 @@ const cli = require('../');
 const { mockLogger } = require('./utils');
 
 const memSink = new sink.MEM();
-const server = new Server({ customSink: memSink, port: 4002 });
+const server = new Server({ customSink: memSink, port: 4005 });
 
 test('Creating a package alias on an asset server', async t => {
     await server.start();
     const l = mockLogger();
 
     await new cli.publish.Dependency({
-        server: `http://localhost:4002`,
+        server: `http://localhost:4005`,
         org: 'my-test-org',
         name: 'lit-html',
         version: '1.1.2'
@@ -22,11 +22,39 @@ test('Creating a package alias on an asset server', async t => {
 
     const result = await new cli.Alias({
         logger: l.logger,
-        server: `http://localhost:4002`,
+        server: `http://localhost:4005`,
         org: 'my-test-org',
         type: 'pkg',
         name: 'lit-html',
         version: '1.1.2',
+        alias: '1'
+    }).run();
+
+    t.equals(result, true, 'Command should return true');
+
+    await server.stop();
+});
+
+test('Creating a map alias on an asset server', async t => {
+    await server.start();
+    const l = mockLogger();
+
+    await new cli.publish.Map({
+        cwd: __dirname,
+        server: `http://localhost:4005`,
+        org: 'my-test-org',
+        name: 'my-map',
+        version: '1.0.0',
+        file: './fixtures/import-map.json'
+    }).run();
+
+    const result = await new cli.Alias({
+        logger: l.logger,
+        server: `http://localhost:4005`,
+        org: 'my-test-org',
+        type: 'map',
+        name: 'my-map',
+        version: '1.0.0',
         alias: '1'
     }).run();
 
