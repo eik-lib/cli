21251c98eccd8b9bb801383661705202e0646da9
Richard Walker <digitalsadhu@gmail.com>
2019-11-25 18:08:27 +1300

    improve error handling in fetch remote hash function

--- utils/fetch-remote-hash.js
+++ utils/fetch-remote-hash.js
@@ -5,12 +5,41 @@ const crypto = require('crypto');
 const fetch = require('node-fetch');
 
 module.exports = async (server, org, name, version) => {
-    const hasher = crypto.createHash('sha512');
     const res = await fetch(`${server}/${join(org, 'pkg', name, version)}`);
-    const body = await res.json();
-    const hashes = body.files.map(file => file.integrity).sort();
-    for (const hash of hashes) {
-        hasher.update(hash);
+
+    if (!res.ok) {
+        throw new Error('Server responded with non 200 status code.');
     }
-    return hasher.digest('base64');
+
+    let body;
+    try {
+        body = await res.json();
+    } catch (err) {
+        throw new Error(
+            'An error occurred while attempting to parse json response from server.',
+        );
+    }
+
+    const { files } = body || {};
+
+    let hashes;
+    try {
+        hashes = files.map(file => file.integrity).sort();
+    } catch (err) {
+        throw new Error(
+            'An error occurred while attempting to map and sort file integrity hashes.',
+        );
+    }
+
+    try {
+        const hasher = crypto.createHash('sha512');
+        for (const hash of hashes) {
+            hasher.update(hash);
+        }
+        return hasher.digest('base64');
+    } catch (err) {
+        throw new Error(
+            'An error occurred while attempting to produce a hash of file integrity hashes.',
+        );
+    }
 };
