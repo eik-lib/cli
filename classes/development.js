182e3bb2e0e7b069f2c143e36ffcc67635088082
Richard Walker <digitalsadhu@gmail.com>
2020-02-07 23:07:22 +1300

    flatten try/catches

--- classes/development.js
+++ classes/development.js
@@ -29,156 +29,161 @@ module.exports = class Meta {
 
     async run() {
         this.log.debug('Creating development asset bundles');
-        const development = {};
-        try {
-            if (this.js) {
-                this.log.debug('Creating js bundle file');
-                try {
-                    const options = {
-                        onwarn: () => {},
-                        plugins: [resolve(), commonjs()],
-                        input: join(this.cwd, this.js),
-                    };
 
-                    if (this.watch) {
-                        const bundled = await rollup.watch({
-                            ...options,
-                            output: {
-                                format: 'esm',
-                                file: join(this.path, 'main/index.js'),
-                                sourcemap: 'inline',
-                            },
-                        });
-                        bundled.on('event', event => {
-                            switch (event.code) {
-                                case 'START':
-                                    this.log.debug(
-                                        'js file watcher is (re)starting',
-                                    );
-                                    break;
-                                case 'END':
-                                    this.log.debug(
-                                        'js file watcher has finished building bundle',
-                                    );
-                                    break;
-                                case 'ERROR':
-                                    this.log.debug(
-                                        'js file watcher encountered an error while bundling',
-                                    );
-                                    break;
-                                default:
-                                    break;
-                            }
-                        });
-                        this.log.debug(
-                            `Development js bundle file generated and saved to ${join(
-                                this.path,
-                                'main/index.js',
-                            )}`,
-                        );
-                    } else {
-                        const bundled = await rollup.rollup(options);
-                        await bundled.write({
+        const development = {};
+        if (this.js) {
+            this.log.debug('Creating js bundle file');
+            try {
+                const options = {
+                    onwarn: () => {},
+                    plugins: [resolve(), commonjs()],
+                    input: join(this.cwd, this.js),
+                };
+
+                if (this.watch) {
+                    const bundled = await rollup.watch({
+                        ...options,
+                        output: {
                             format: 'esm',
                             file: join(this.path, 'main/index.js'),
                             sourcemap: 'inline',
-                        });
-                        this.log.debug(
-                            `Development js bundle file generated and saved to ${join(
-                                this.path,
-                                'main/index.js',
-                            )}`,
-                        );
-                    }
-                    development.js = join(this.path, 'main/index.js');
-                } catch (err) {
-                    throw new Error(
-                        `Unable to create js bundle file: ${err.message}`,
+                        },
+                    });
+                    bundled.on('event', event => {
+                        switch (event.code) {
+                            case 'START':
+                                this.log.debug(
+                                    'js file watcher is (re)starting',
+                                );
+                                break;
+                            case 'END':
+                                this.log.debug(
+                                    'js file watcher has finished building bundle',
+                                );
+                                break;
+                            case 'ERROR':
+                                this.log.debug(
+                                    'js file watcher encountered an error while bundling',
+                                );
+                                break;
+                            default:
+                                break;
+                        }
+                    });
+                    this.log.debug(
+                        `Development js bundle file generated and saved to ${join(
+                            this.path,
+                            'main/index.js',
+                        )}`,
+                    );
+                } else {
+                    const bundled = await rollup.rollup(options);
+                    await bundled.write({
+                        format: 'esm',
+                        file: join(this.path, 'main/index.js'),
+                        sourcemap: 'inline',
+                    });
+                    this.log.debug(
+                        `Development js bundle file generated and saved to ${join(
+                            this.path,
+                            'main/index.js',
+                        )}`,
                     );
                 }
+                development.js = join(this.path, 'main/index.js');
+            } catch (err) {
+                this.log.error(
+                    `Unable to create js bundle file: ${err.message}`,
+                );
+                this.log.warn(err.message);
+                return false;
             }
+        } else {
+            this.log.debug('JS assets not specified');
+        }
 
-            if (this.css) {
-                try {
-                    this.log.debug('Creating css bundle file');
+        if (this.css) {
+            try {
+                this.log.debug('Creating css bundle file');
 
-                    const input = join(this.cwd, this.css);
-                    const processor = postcss(autoprefixer());
+                const input = join(this.cwd, this.css);
+                const processor = postcss(autoprefixer());
 
-                    processor.use(atImport());
-                    processor.use(cssnano());
+                processor.use(atImport());
+                processor.use(cssnano());
 
-                    if (this.watch) {
-                        chokidar
-                            .watch('**/*.css', {
-                                ignored: /node_modules/,
-                                cwd: this.cwd,
-                                followSymlinks: false,
-                                persistent: true,
-                            })
-                            .on('all', async () => {
-                                this.log.debug(
-                                    'css file watcher is (re)starting',
-                                );
-                                const precss = fs.readFileSync(input, 'utf8');
-                                const result = await processor.process(precss, {
-                                    from: this.css.replace(/(.*\/)*/, ''),
-                                    to: 'index.css',
-                                    map: { inline: true },
-                                });
-                                fs.writeFileSync(
-                                    join(this.path, 'main/index.css'),
-                                    result.css,
-                                );
-                                this.log.debug(
-                                    'file watcher has finished building css bundle',
-                                );
+                if (this.watch) {
+                    chokidar
+                        .watch('**/*.css', {
+                            ignored: /node_modules/,
+                            cwd: this.cwd,
+                            followSymlinks: false,
+                            persistent: true,
+                        })
+                        .on('all', async () => {
+                            this.log.debug('css file watcher is (re)starting');
+                            const precss = fs.readFileSync(input, 'utf8');
+                            const result = await processor.process(precss, {
+                                from: this.css.replace(/(.*\/)*/, ''),
+                                to: 'index.css',
+                                map: { inline: true },
                             });
-                        this.log.debug(
-                            `Development css bundle file generated and saved to ${join(
-                                this.path,
-                                'main/index.css',
-                            )}`,
-                        );
-                    } else {
-                        const precss = fs.readFileSync(input, 'utf8');
-                        const result = await processor.process(precss, {
-                            from: this.css.replace(/(.*\/)*/, ''),
-                            to: 'index.css',
-                            map: { inline: true },
+                            fs.writeFileSync(
+                                join(this.path, 'main/index.css'),
+                                result.css,
+                            );
+                            this.log.debug(
+                                'file watcher has finished building css bundle',
+                            );
                         });
-                        fs.writeFileSync(
-                            join(this.path, 'main/index.css'),
-                            result.css,
-                        );
-                        this.log.debug(
-                            `Development css bundle file generated and saved to ${join(
-                                this.path,
-                                'main/index.css',
-                            )}`,
-                        );
-                    }
-                    development.css = join(this.path, 'main/index.css');
-                } catch (err) {
-                    throw new Error(
-                        `Unable to create css bundle file: ${err.message}`,
+                    this.log.debug(
+                        `Development css bundle file generated and saved to ${join(
+                            this.path,
+                            'main/index.css',
+                        )}`,
+                    );
+                } else {
+                    const precss = fs.readFileSync(input, 'utf8');
+                    const result = await processor.process(precss, {
+                        from: this.css.replace(/(.*\/)*/, ''),
+                        to: 'index.css',
+                        map: { inline: true },
+                    });
+                    fs.writeFileSync(
+                        join(this.path, 'main/index.css'),
+                        result.css,
+                    );
+                    this.log.debug(
+                        `Development css bundle file generated and saved to ${join(
+                            this.path,
+                            'main/index.css',
+                        )}`,
                     );
                 }
-            } else {
-                this.log.debug('CSS assets not specified');
+                development.css = join(this.path, 'main/index.css');
+            } catch (err) {
+                this.log.error(
+                    `Unable to create css bundle file: ${err.message}`,
+                );
+                this.log.warn(err.message);
+                return false;
             }
+        } else {
+            this.log.debug('CSS assets not specified');
+        }
 
-            if (development.js || development.css) {
+        if (development.js || development.css) {
+            try {
                 const meta = await readMetaFile();
                 meta.development = development;
                 await writeMetaFile(meta);
                 this.log.debug('.eikrc metafile saved to disk');
                 return true;
+            } catch (err) {
+                this.log.error('Unable to save .eikrc metafile to disk');
+                this.log.warn(err.message);
+                return false;
             }
-        } catch (err) {
-            this.log.error('Unable to bundle assets for development');
-            this.log.warn(err.message);
-            return false;
         }
     }
 };
