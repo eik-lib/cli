a09692bfb311bf039ddc35d0db2634825b51380a
Richard Walker <digitalsadhu@gmail.com>
2020-04-16 11:20:30 +1200

    add ping command

---
+++ classes/ping.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const abslog = require('abslog');
+const { validators } = require('@eik/common');
+
+module.exports = class Ping {
+    constructor({ logger, server } = {}) {
+        this.log = abslog(logger);
+        this.server = server;
+    }
+
+    async run() {
+        this.log.debug('Validating input');
+
+        try {
+            validators.origin(this.server);
+        } catch (err) {
+            this.log.error(`Parameter "server" is not valid`);
+            return false;
+        }
+
+        this.log.debug('Requesting ping from server');
+        try {
+            const result = await fetch(this.server);
+
+            if(!result.ok) {
+                const err = new Error('Ping unsuccessful');
+                err.statusCode = result.status;
+                throw err;
+            }
+
+            this.log.info(`Ping successful`);
+            return true;
+        } catch (err) {
+            if (err.code === 'ENOTFOUND') {
+                this.log.info('Ping unsuccessful. Server not found.');
+                return false;
+            }
+
+            switch (err.statusCode) {
+                case 404:
+                    this.log.info('Ping unsuccessful. Route not found.');
+                    return false;
+                default:
+                    this.log.warn('Ping unsuccessful. Unknown server error');
+                    return false;
+            }
+        }
+    }
+};
---
+++ commands/ping.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const ora = require('ora');
+const { readFileSync } = require('fs');
+const Ping = require('../classes/ping');
+const { resolvePath, logger } = require('../utils');
+
+exports.command = 'ping';
+
+exports.aliases = [];
+
+exports.describe = `Ping an Eik server`;
+
+exports.builder = (yargs) => {
+    let assets = {};
+    try {
+        const assetsPath = resolvePath('./assets.json').pathname;
+        assets = JSON.parse(readFileSync(assetsPath));
+    } catch (err) {
+        // noop
+    }
+
+    yargs.options({
+        server: {
+            alias: 's',
+            describe: 'Specify location of eik server.',
+            default: assets.server || '',
+        },
+        debug: {
+            describe: 'Logs additional messages',
+            default: false,
+            type: 'boolean',
+        },
+    });
+};
+
+exports.handler = async (argv) => {
+    const spinner = ora().start('working...');
+    let success = false;
+    const { debug } = argv;
+
+    try {
+        success = await new Ping({
+            logger: logger(spinner, debug),
+            ...argv,
+        }).run();
+    } catch (err) {
+        logger.warn(err.message);
+    }
+
+    if (success) {
+        spinner.text = '';
+        spinner.stopAndPersist();
+    } else {
+        spinner.text = '';
+        spinner.stopAndPersist();
+        process.exit(1);
+    }
+};
