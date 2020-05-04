24c2c42ef1f19bb42ee178428e86743daadf124e
Richard Walker <digitalsadhu@gmail.com>
2020-05-04 14:39:03 +1200

    refactor utils read and write meta file functions

--- utils/read-meta-file.js
+++ utils/read-meta-file.js
@@ -9,11 +9,6 @@ module.exports = async ({ cwd = process.cwd(), filename = '.eikrc' } = {}) => {
         const meta = await fs.readFile(path, 'utf8');
         return JSON.parse(meta);
     } catch (err) {
-        return {
-            token: null,
-            version: null,
-            integrity: null,
-            development: { js: '', css: '' },
-        };
+        return {};
     }
 };
--- utils/write-meta-file.js
+++ utils/write-meta-file.js
@@ -3,22 +3,12 @@
 const fs = require('fs').promises;
 const { join } = require('path');
 
-module.exports = async ({
-    version,
-    token,
-    integrity,
-    development = { js: '', css: '' },
-}, {
+module.exports = async (meta = {}, {
     cwd = process.cwd(),
     filename = '.eikrc',
 }) => {
     await fs.writeFile(
         join(cwd, filename),
-        JSON.stringify({
-            version,
-            integrity,
-            development,
-            token,
-        }),
+        JSON.stringify(meta),
     );
 };
