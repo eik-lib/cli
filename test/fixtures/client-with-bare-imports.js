eaf9647553d2d3266f5aaa36e386e536f3b69e91
Richard Walker <digitalsadhu@gmail.com>
2020-05-05 13:47:56 +1200

    add workflow test for publishing using import maps

---
+++ test/fixtures/client-with-bare-imports.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-unresolved */
+
+'use strict';
+
+import scrollIntoView from 'scroll-into-view-if-needed'
+
+
+const main = () => {
+    // eslint-disable-next-line no-undef
+    const node = document.getElementById('hero')
+    
+    scrollIntoView(node, {
+        scrollMode: 'if-needed',
+        block: 'nearest',
+        inline: 'nearest',
+    });
+};
+
+main();
