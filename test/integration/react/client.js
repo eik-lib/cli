d5e0370c9047c2cba9eeef21d699d87ce046d57c
Richard Walker <digitalsadhu@gmail.com>
2020-05-13 10:20:13 +1200

    add full workflow test for react (esm and ie11)

---
+++ test/integration/react/client.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function main() {
+    const domContainer = document.querySelector('#container');
+    ReactDOM.render(
+        React.createElement(
+            'button',
+            { 
+                onClick: () => {
+                    document.querySelector('#success').innerHTML = 'true';
+                }
+            },
+            'Like'
+        ),
+        domContainer
+    );
+}
+
+main();
