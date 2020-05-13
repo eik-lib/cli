5eb418e97a7c5194f2c8d303f2c86fa6c6f74b89
Richard Walker <digitalsadhu@gmail.com>
2020-05-13 14:03:29 +1200

    improve command line docs for init command

--- commands/init.js
+++ commands/init.js
@@ -8,7 +8,8 @@ exports.command = 'init';
 
 exports.aliases = ['i'];
 
-exports.describe = `Create a new assets.json file in the current working directory`;
+exports.describe = `Creates a new default "assets.json" file and saves it to the current working directory
+    Override default "assets.json" fields using command line flags --server, --name, --major, --js and --css`;
 
 exports.builder = (yargs) => {
     yargs.example('eik init');
@@ -19,36 +20,46 @@ exports.builder = (yargs) => {
     yargs.options({
         server: {
             alias: 's',
-            describe: 'Specify location of asset server.',
+            describe: `Specify asset server field in "assets.json".
+                This the URL to an Eik asset server
+                Eg. --server https://assets.myeikserver.com`,
             default: '',
         },
         cwd: {
             alias: 'c',
-            describe: 'Alter current working directory.',
+            describe: `Alter the current working directory
+                Defaults to the directory where the command is being run.
+                This affects where the generated "assets.json" file will be saved.
+                Eg. --cwd /path/to/save/to`,
             default: process.cwd(),
         },
         major: {
             alias: 'm',
-            describe: 'Specify the semver major version for the package.',
+            describe: `Specify the semver major version field in "assets.json".
+                This should be a single integer. 
+                Eg. --major 2`,
             default: 1,
         },
         name: {
             alias: 'n',
-            describe: 'Specify the app name context for the command.',
+            describe: `Specify the app name field in "assets.json".
+                Eg. --name my-great-app`,
             default: '',
         },
         js: {
             describe:
-                'Specify the path on local disk to JavaScript client side assets relative to the current working directory.',
+                `Specify the path on local disk to JavaScript client side assets relative to the current working directory.
+                This will be used to populate the "js.input" field of "assets.json"`,
             default: '',
         },
         css: {
             describe:
-                'Specify the path on local disk to CSS client side assets relative to the current working directory.',
+                `Specify the path on local disk to CSS client side assets relative to the current working directory.
+                This will be used to populate the "css.input" field of "assets.json"`,
             default: '',
         },
         debug: {
-            describe: 'Logs additional messages',
+            describe: 'Logs additional messages during command run',
             default: false,
             type: 'boolean',
         },
