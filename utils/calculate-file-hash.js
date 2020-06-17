4ce043574300d503b0a8aa6d83dd559e7382f856
Richard Walker <digitalsadhu@gmail.com>
2020-06-17 14:20:58 +1200

    fix: use ssri module to stabilize hashing

--- utils/calculate-file-hash.js
+++ utils/calculate-file-hash.js
@@ -1,16 +1,9 @@
 'use strict';
 
-const crypto = require('crypto');
+const ssri = require('ssri');
 const fs = require('fs');
-const { pipeline } = require('stream');
 
-module.exports = path =>
-    new Promise((resolve, reject) => {
-        const hash = crypto.createHash('sha512');
-        const file = fs.createReadStream(path);
-
-        pipeline(file, hash, error => {
-            if (error) return reject(error);
-            return resolve(`sha512-${hash.digest('base64')}`);
-        });
-    });
+module.exports = async path => {
+    const integrity = await ssri.fromStream(fs.createReadStream(path));
+    return integrity.toString();
+}
