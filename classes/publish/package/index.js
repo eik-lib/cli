d4777ea612bb93292a13f157bfb4d731e093106e
Richard Walker <digitalsadhu@gmail.com>
2020-06-26 09:06:47 +1200

    fix: do not check for bundle existence on dry run

--- classes/publish/package/index.js
+++ classes/publish/package/index.js
@@ -1,16 +1,12 @@
 'use strict';
 
 const abslog = require('abslog');
-// const tempDir = require('temp-dir');
 const { join, isAbsolute } = require('path');
 const ValidateInput = require('./tasks/validate-input');
 const CreateTempDirectory = require('./tasks/create-temp-directory');
-// const FetchImportMaps = require('./tasks/fetch-import-maps');
-// const CreateBundles = require('./tasks/create-bundles');
 const CreateZipFile = require('./tasks/create-zip-file');
 const CheckBundleSizes = require('./tasks/check-bundle-sizes');
 const DryRun = require('./tasks/dry-run');
-// const FetchVersion = require('./tasks/fetch-version');
 const CheckIfAlreadyPublished = require('./tasks/check-if-already-published');
 const UploadFiles = require('./tasks/upload-files');
 const SaveMetafile = require('./tasks/save-metafile');
@@ -57,12 +53,9 @@ module.exports = class PublishApp {
         this.path = isAbsolute(out) ? out : join(cwd, out);
         this.validateInput = new ValidateInput(this.log);
         this.createTempDirectory = new CreateTempDirectory(this.log);
-        // this.fetchImportMaps = new FetchImportMaps(this.log);
-        // this.createBundles = new CreateBundles(this.log);
         this.createZipFile = new CreateZipFile(this.log);
         this.checkBundleSizes = new CheckBundleSizes(this.log);
         this.runDryRun = new DryRun(this.log);
-        // this.fetchVersion = new FetchVersion(this.log);
         this.checkIfAlreadyPublished = new CheckIfAlreadyPublished(this.log);
         this.uploadFiles = new UploadFiles(this.log);
         this.saveMetafile = new SaveMetafile(this.log);
@@ -104,17 +97,15 @@ module.exports = class PublishApp {
 
         await this.validateInput.process(incoming, outgoing);
         await this.createTempDirectory.process(incoming, outgoing);
-        // await this.fetchImportMaps.process(incoming, outgoing);
-        // await this.createBundles.process(incoming, outgoing);
         await this.createZipFile.process(incoming, outgoing);
         await this.checkBundleSizes.process(incoming, outgoing);
-        // await this.fetchVersion.process(incoming, outgoing);
-        await this.checkIfAlreadyPublished.process(incoming, outgoing);
 
         if (this.dryRun) {
             await this.runDryRun.process(incoming, outgoing);
             return outgoing;
         }
+
+        await this.checkIfAlreadyPublished.process(incoming, outgoing);
             
         await this.uploadFiles.process(incoming, outgoing);
         await this.saveMetafile.process(incoming, outgoing);
