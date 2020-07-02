651a267f97d6bac852deeead57aa41763ca7ed5c
Richard Walker <digitalsadhu@gmail.com>
2020-07-02 23:02:22 +1200

    feat: add input validation

--- classes/version.js
+++ classes/version.js
@@ -1,15 +1,27 @@
+/* eslint-disable max-classes-per-file */
+
 'use strict';
 
 const { copyFileSync, writeFileSync } = require('fs');
-const { join, isAbsolute } = require('path');
+const { join, isAbsolute, parse } = require('path');
 const abslog = require('abslog');
 const semver = require('semver');
-// const { validators } = require('@eik/common');
+const { validators } = require('@eik/common');
 const {
     fetchPackageMeta,
     calculateFilesHash,
     compareHashes,
 } = require('../utils');
+
+class ValidationError extends Error {
+    constructor(message, err) {
+        let m = message;
+        if (err && err.message) m += `: ${err.message}`;
+        super(m);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
 
 module.exports = class Ping {
     constructor({
@@ -48,15 +60,77 @@ module.exports = class Ping {
     }
 
     async run() {
-        // TODO: validate input
-        // version exists and is semver
-        // server is a url
-        // js or css files exist
-        // name is a string
-        // level is major, minor or patch
+        const {
+            log,
+            server,
+            name,
+            version,
+            level,
+            cwd,
+            js,
+            css,
+            map,
+            path,
+            out,
+        } = this;
 
-        const { log, server, name, version, level, cwd, js, css, map, path, out } = this;
-        
+        log.debug('Validating input');
+
+        try {
+            log.debug(`  ==> cwd: ${cwd}`);
+            parse(cwd);
+        } catch (err) {
+            throw new ValidationError('Parameter "cwd" is not valid', err);
+        }
+
+        try {
+            log.debug(`  ==> server: ${server}`);
+            validators.origin(server);
+        } catch (err) {
+            throw new ValidationError(`Parameter "server" is not valid`, err);
+        }
+
+        try {
+            log.debug(`  ==> name: ${name}`);
+            validators.name(name);
+        } catch (err) {
+            throw new ValidationError('Parameter "name" is not valid', err);
+        }
+
+        try {
+            log.debug(`  ==> version: ${version}`);
+            validators.version(version);
+        } catch (err) {
+            throw new ValidationError('Parameter "version" is not valid', err);
+        }
+
+        log.debug(`  ==> level: ${level}`);
+        if (!['major', 'minor', 'patch'].includes(level)) {
+            throw new ValidationError('Parameter "version" is not valid');
+        }
+
+        log.debug(`  ==> at least 1 of js or css: ${!!js || !!css}`);
+        if (!js && !css) {
+            throw new ValidationError(
+                'At least one of "js" or "css" must be provided',
+            );
+        }
+
+        log.debug(`  ==> js: ${JSON.stringify(js)}`);
+        if (js && typeof js !== 'string' && typeof js !== 'object') {
+            throw new ValidationError('Parameter "js" is not valid');
+        }
+
+        log.debug(`  ==> css: ${JSON.stringify(css)}`);
+        if (css && typeof css !== 'string' && typeof css !== 'object') {
+            throw new ValidationError('Parameter "css" is not valid');
+        }
+
+        log.debug(`  ==> map: ${JSON.stringify(map)}`);
+        if (!Array.isArray(map)) {
+            throw new ValidationError('Parameter "map" is not valid');
+        }
+
         log.debug('Checking local package version');
         log.debug(`Current local package version determined to be ${version}`);
         log.debug(`Fetching remote package metadata from ${server}`);
@@ -92,7 +166,7 @@ module.exports = class Ping {
             };
             writeFileSync(eikPathDest, JSON.stringify(eikJSON, null, 2));
 
-            const localFiles = [ eikPathDest ];
+            const localFiles = [eikPathDest];
             log.debug(`  ==> ${eikPathDest}`);
 
             if (js) {
@@ -121,7 +195,7 @@ module.exports = class Ping {
                     // throw new Error(`Failed to zip CSS: ${err.message}`);
                 }
             }
-            
+
             localHash = await calculateFilesHash(localFiles);
         } catch (err) {
             throw new Error(
@@ -129,7 +203,7 @@ module.exports = class Ping {
             );
         }
 
-        log.debug(`Comparing hashes:`)
+        log.debug(`Comparing hashes:`);
         log.debug(`  ==> local: ${localHash}`);
         log.debug(`  ==> remote: ${meta.integrity}`);
         const same = compareHashes(meta.integrity, localHash);
