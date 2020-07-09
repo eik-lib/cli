0c87a6e410947fe124dcb2b2e7e7b3d27b62b4c4
Richard Walker <digitalsadhu@gmail.com>
2020-07-09 17:47:44 +1200

    feat: add integrity command and tests

---
+++ commands/integrity.js
@@ -0,0 +1,92 @@
+/* eslint-disable prefer-template */
+/* eslint-disable no-restricted-properties */
+/* eslint-disable one-var */
+
+'use strict';
+
+const { join } = require('path');
+const ora = require('ora');
+const Integrity = require('../classes/integrity');
+const { logger, getDefaults, getCWD, writeJSON } = require('../utils');
+
+exports.command = 'integrity [name] [version]';
+
+exports.aliases = ['int'];
+
+exports.describe = `Retrieve file integrity information for package name and version defined in eik.json, then populate integrity.json file with this information`;
+
+exports.builder = (yargs) => {
+    const cwd = getCWD();
+    const defaults = getDefaults(cwd);
+
+    yargs.positional('name', {
+        describe: 'Name of package. Defaults to value defined in eik.json',
+        type: 'string',
+        default: defaults.name,
+    });
+
+    yargs.positional('version', {
+        describe: 'Semver version of package. Defaults to value defined in eik.json',
+        type: 'string',
+        default: defaults.version,
+    });
+
+    yargs.options({
+        server: {
+            alias: 's',
+            describe: 'Specify location of asset server.',
+            default: defaults.server,
+        },
+        debug: {
+            describe: 'Logs additional messages',
+            default: false,
+            type: 'boolean',
+        },
+        cwd: {
+            alias: 'c',
+            describe: 'Alter current working directory.',
+            default: defaults.cwd,
+        },
+        out: {
+            alias: 'o',
+            describe: 'Eik directory',
+            default: defaults.out,
+        },
+    });
+
+    yargs.example(`eik integrity`);
+    yargs.example(`eik integrity my-package 1.0.0`);
+    yargs.example(`eik integrity --debug`);
+    yargs.example(`eik integrity --server https://assets.myeikserver.com`);
+};
+
+exports.handler = async (argv) => {
+    const spinner = ora({ stream: process.stdout }).start('working...');
+    let integrity = false;
+    const { debug, server, cwd, name, version, out } = argv;
+    const l = logger(spinner, debug);
+
+    try {
+        integrity = await new Integrity({ 
+            logger: l,
+            name,
+            version,
+            server,
+            debug,
+            cwd,
+        }).run();
+
+        if (integrity) {
+            const filename = join(out, 'integrity.json');
+            await writeJSON(integrity, { cwd, filename });
+            // print "integrity information for package x at version y saved to location z"
+            spinner.succeed(`integrity information for package "${name}" (v${version}) saved to "${filename}"`);
+            process.stdout.write('\n');
+        }
+    } catch (err) {
+        spinner.text = '';
+        spinner.stopAndPersist();
+        l.warn(err.message);
+        process.exit(1);
+    }
+};
---
+++ test/integration/integrity.test.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/* eslint-disable no-param-reassign */
+const fastify = require('fastify');
+const fs = require('fs').promises;
+const os = require('os');
+const cp = require('child_process');
+const { join } = require('path');
+const { test, beforeEach, afterEach } = require('tap');
+const EikService = require('@eik/service');
+const { sink } = require('@eik/core');
+const cli = require('../..');
+
+function exec(cmd) {
+    return new Promise((resolve) => {
+        cp.exec(cmd, (error, stdout, stderr) => {
+            resolve({ error, stdout, stderr });
+        });
+    });
+}
+
+beforeEach(async (done, t) => {
+    const memSink = new sink.MEM();
+    const server = fastify({ logger: false });
+    const service = new EikService({ customSink: memSink });
+    server.register(service.api());
+    const address = await server.listen();
+    const folder = await fs.mkdtemp(join(os.tmpdir(), 'foo-'));
+    
+    const token = await new cli.Login({
+        server: address,
+        key: 'change_me',
+    }).run();
+
+    t.context.server = server;
+    t.context.address = address;
+    t.context.folder = folder;
+    t.context.token = token;
+    done();
+});
+
+afterEach(async (done, t) => {
+    await t.context.server.close();
+    done();
+});
+
+test('eik meta : details provided by eik.json', async (t) => {
+    const assets = {
+        name: 'test-app',
+        version: '1.0.0',
+        server: t.context.address,
+        js: join(__dirname, '..', 'fixtures', 'client.js'),
+        css: join(__dirname, '..', 'fixtures', 'styles.css'),
+    };
+    await fs.writeFile(
+        join(t.context.folder, 'eik.json'),
+        JSON.stringify(assets),
+    );
+
+    const eik = join(__dirname, '../../index.js');
+
+    let cmd = `${eik} package --token ${t.context.token} --cwd ${t.context.folder}`;
+    await exec(cmd);
+    
+    cmd = `${eik} integrity --cwd ${t.context.folder}`;
+    const { error, stdout } = await exec(cmd);
+
+    const integrity = JSON.parse(await fs.readFile(join(t.context.folder, './.eik/integrity.json'), 'utf8'));
+
+    t.notOk(error);
+    t.match(stdout, 'integrity information for package "test-app" (v1.0.0) saved to ".eik/integrity.json"');
+    t.equal(integrity.name, 'test-app');
+    t.equal(integrity.version, '1.0.0');
+    t.ok(integrity.integrity);
+    t.end();
+});
