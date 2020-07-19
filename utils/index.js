cb35c1b3c5485dd9b72865a5f604623fdf28ace3
Richard Walker <digitalsadhu@gmail.com>
2020-07-19 20:06:23 +0200

    chore: remove unused readAssetsJson function

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const resolvePath = require('./resolve-path');
-const readAssetsJson = require('./read-assets-json');
 const sendCommand = require('./send-command');
-const parseInput = require('./parse-input');
 const logger = require('./logger');
 const calculateFileHash = require('./calculate-file-hash');
 const calculateFilesHash = require('./calculate-files-hash');
@@ -22,9 +20,7 @@ const writeEikJSON = require('./write-eik-json');
 
 module.exports = {
     resolvePath,
-    readAssetsJson,
     sendCommand,
-    parseInput,
     logger,
     calculateFileHash,
     calculateFilesHash,
