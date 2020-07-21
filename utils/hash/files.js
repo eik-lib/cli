e3edef11fef5586b25fce0a56036e90c384d3b71
Richard Walker <digitalsadhu@gmail.com>
2020-07-21 13:45:18 +0200

    docs: add jsdoc comments to utils

--- utils/hash/files.js
+++ utils/hash/files.js
@@ -3,6 +3,15 @@
 const ssri = require('ssri');
 const fileHash = require('./file');
 
+/**
+ * Reads files from given paths and produces and returns an integrity hash from all files contents
+ *
+ * @param {[string]} files - an array of file paths
+ *
+ * @returns {Promise<string>} - integrity string
+ * 
+ * @example hash.files(['/path/to/file1.js', '/path/to/file2.js']);
+ */
 module.exports = async files => {
     const hashes = await Promise.all(files.map(fileHash));
     const hasher = ssri.create();
