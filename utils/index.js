ac404b4fcb9ee6594e38023c990d2626bdc0f08b
Richard Walker <digitalsadhu@gmail.com>
2020-08-17 23:04:56 +0200

    feat: refactor to support entrypoints object with globbing

--- utils/index.js
+++ utils/index.js
@@ -3,5 +3,6 @@
 const logger = require('./logger');
 const getDefaults = require('./get-defaults');
 const getCWD = require('./get-cwd');
+const entrypoints = require('./entrypoints');
 
-module.exports = { logger, getDefaults, getCWD };
+module.exports = { logger, getDefaults, getCWD, entrypoints };
