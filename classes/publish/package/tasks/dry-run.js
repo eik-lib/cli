b423ba1f548261f83f8ca0cc069143b3b472642e
Richard Walker <digitalsadhu@gmail.com>
2020-08-18 13:10:49 +0200

    fix: update dry run command output to match new entrypoints work

--- classes/publish/package/tasks/dry-run.js
+++ classes/publish/package/tasks/dry-run.js
@@ -2,20 +2,22 @@
 
 'use strict';
 
-const {join} = require('path');
 const Task = require('./task');
+const { entrypoints: mapEntrypoints } = require('../../../../utils');
 
 module.exports = class DryRun extends Task {
     async process(incoming = {}, outgoing = {}) {
-        const { dryRun, path, zipFile, entrypoints } = incoming;
+        const { dryRun, path, zipFile, entrypoints, cwd } = incoming;
         if (dryRun) {
             outgoing.files = [
                 { pathname: path, type: 'temporary directory' },
                 { pathname: zipFile, type: 'package archive' },
             ]
 
-            for (const key of Object.keys(entrypoints)) {
-                outgoing.files.push({ pathname: join(path, key), type: 'package file' })
+            const files = await mapEntrypoints(entrypoints, path, { cwd });
+
+            for (const [, dest] of files) {
+                outgoing.files.push({ pathname: dest, type: 'package file' });
             }
         }
 
