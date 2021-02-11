89fe9c766097c52f52f1addf9c6a300a190eda46
Richard <digitalsadhu@gmail.com>
2021-02-11 08:40:07 +0100

    chore: split cli script and programmatic api script

--- index.js
+++ index.js
@@ -7,42 +7,35 @@ const yargs = require('yargs');
 const boxen = require('boxen');
 const { join } = require('path');
 const { readFileSync } = require('fs');
-const classes = require('./classes');
 
-const runningAsScript = !module.parent;
+const { version } = JSON.parse(
+    readFileSync(join(__dirname, './package.json')),
+);
+const greeting = chalk.white.bold(`Eik CLI (v${version})`);
 
-module.exports = classes;
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'green',
+    backgroundColor: '#555555',
+};
+const msgBox = boxen(greeting, boxenOptions);
 
-if (runningAsScript) {
-    const { version } = JSON.parse(
-        readFileSync(join(__dirname, './package.json')),
-    );
-    const greeting = chalk.white.bold(`Eik CLI (v${version})`);
+// eslint-disable-next-line no-console
+console.log(msgBox);
 
-    const boxenOptions = {
-        padding: 1,
-        margin: 1,
-        borderStyle: 'round',
-        borderColor: 'green',
-        backgroundColor: '#555555',
-    };
-    const msgBox = boxen(greeting, boxenOptions);
-
-    // eslint-disable-next-line no-console
-    console.log(msgBox);
-
-    // eslint-disable-next-line no-unused-expressions
-    yargs
-        .example('eik init')
-        .example('eik login --server https://assets.myserver.com --key ######')
-        .example('eik publish')
-        .example('eik meta my-app --server https://assets.myserver.com')
-        .example('eik npm-alias lit-html 1.0.0 1 --server https://assets.myserver.com --token ######')
-        .example('eik map my-map 1.0.0 ./import-map.json --server https://assets.myserver.com --token ######')
-        .example('eik map-alias my-map 1.0.0 1')
-        .commandDir('commands')
-        .demandCommand()
-        .wrap(150)
-        .version(false)
-        .help().argv;
-}
+// eslint-disable-next-line no-unused-expressions
+yargs
+    .example('eik init')
+    .example('eik login --server https://assets.myserver.com --key ######')
+    .example('eik publish')
+    .example('eik meta my-app --server https://assets.myserver.com')
+    .example('eik npm-alias lit-html 1.0.0 1 --server https://assets.myserver.com --token ######')
+    .example('eik map my-map 1.0.0 ./import-map.json --server https://assets.myserver.com --token ######')
+    .example('eik map-alias my-map 1.0.0 1')
+    .commandDir('commands')
+    .demandCommand()
+    .wrap(150)
+    .version(false)
+    .help().argv;
