1c4788f5ab40733f24b49321e39ea2886600f5d4
Trygve Lie <trygve.lie@finn.no>
2021-04-18 20:58:35 +0200

    ci: Fix semantic release config formating

--- release.config.js
+++ release.config.js
@@ -1,65 +1,69 @@
 module.exports = {
   plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      [
-          '@semantic-release/npm',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'release',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'release/*.tgz',
+      },
+    ],
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: false,
+        notifyOnFail: false,
+        packageName: '@eik/cli',
+        branchesConfig: [
           {
-              tarballDir: 'release',
+            pattern: 'master',
+            notifyOnSuccess: true,
+            onSuccessTemplate: {
+              text:
+                '$package_name $npm_package_version is now available - $repo_url',
+            },
           },
-      ],
-      [
-          '@semantic-release/github',
           {
-              assets: 'release/*.tgz',
+            pattern: 'alpha',
+            notifyOnSuccess: true,
+            onSuccessTemplate: {
+              text:
+                '$package_name $npm_package_version (pre-release) is now available - $repo_url',
+            },
           },
-      ],
-      [
-          'semantic-release-slack-bot',
           {
-              notifyOnSuccess: false,
-              notifyOnFail: false,
-              packageName: '@eik/cli',
-              branchesConfig: [
-                  {
-                      pattern: 'master',
-                      notifyOnSuccess: true,
-                      onSuccessTemplate: {
-                          text: '$package_name $npm_package_version is now available - $repo_url',
-                      },
-                  },
-                  {
-                      pattern: 'alpha',
-                      notifyOnSuccess: true,
-                      onSuccessTemplate: {
-                          text: '$package_name $npm_package_version (pre-release) is now available - $repo_url',
-                      },
-                  },
-                  {
-                      pattern: 'beta',
-                      notifyOnSuccess: true,
-                      onSuccessTemplate: {
-                          text: '$package_name $npm_package_version (pre-release) is now available - $repo_url',
-                      },
-                  },
-                  {
-                      pattern: 'next',
-                      notifyOnSuccess: true,
-                      onSuccessTemplate: {
-                          text: '$package_name $npm_package_version (pre-release) is now available - $repo_url',
-                      },
-                  },
-              ]
-          }
-      ],
-      '@semantic-release/git',
+            pattern: 'beta',
+            notifyOnSuccess: true,
+            onSuccessTemplate: {
+              text:
+                '$package_name $npm_package_version (pre-release) is now available - $repo_url',
+            },
+          },
+          {
+            pattern: 'next',
+            notifyOnSuccess: true,
+            onSuccessTemplate: {
+              text:
+                '$package_name $npm_package_version (pre-release) is now available - $repo_url',
+            },
+          },
+        ],
+      },
+    ],
+    '@semantic-release/git',
   ],
   preset: 'angular',
   branches: [
-      { name: 'master' },
-      { name: 'alpha', prerelease: true },
-      { name: 'beta', prerelease: true },
-      { name: 'next', prerelease: true },
+    { name: 'master' },
+    { name: 'alpha', prerelease: true },
+    { name: 'beta', prerelease: true },
+    { name: 'next', prerelease: true },
   ],
 };
