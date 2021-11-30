3d1f4acaa62a63d8a064542f3107f5607e2896a7
Trygve Lie <trygve.lie@finn.no>
2021-11-30 11:19:07 +0100

    chore(deps): Update all non breaking dependencies

--- formatters/file.js
+++ formatters/file.js
@@ -11,7 +11,7 @@ const chalk = require('chalk');
 function readableBytes(bytes) {
     const i = Math.floor(Math.log(bytes) / Math.log(1024)),
         sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    return (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + sizes[i];
+    return (bytes / 1024**i).toFixed(2) * 1 + ' ' + sizes[i];
 }
 
 class File {
